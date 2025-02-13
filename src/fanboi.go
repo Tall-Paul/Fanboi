4f589bb4a6cc16b817e33c5491eca617d2eec3e9
PaulBB <tall_paulb@hotmail.com>
2025-02-13 21:00:57 +0000

    updated main

--- src/fanboi.go
+++ src/fanboi.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"fanboi/plugin"
-	"fmt"
 	"log"
 )
 
 func main() {
 	pluginDir := "./plugins"
+	cache := make(map[string]map[string]float32)
 
 	pm, err := plugin.LoadPlugins(pluginDir)
+	for pluginName := range pm.ListTemplatePlugins() {
+		cache[pluginName] = make(map[string]float32)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	temperature, err := pm.ApplyGetHook("unraiddrives", "drive1")
+
+	//this bit is just to test the template stuff works
+	fan1Temp, err := pm.ApplyGetHook("echo", "one")
 	if err == nil {
-		fmt.Printf("Plugin returned %f", temperature)
-	} else {
-		log.Fatal(err)
+		cache["template"]["fan1"] = fan1Temp
 	}
+	fan2Temp, err := pm.ApplyGetHook("echo", "two")
+	if err == nil {
+		cache["template"]["fan2"] = fan2Temp
+	}
+
+	pm.WriteTemplateHook("template", cache["template"])
 
 }
