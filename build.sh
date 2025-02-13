ace00c0ff6b41e1119670bac797cbd9392b15bcd
PaulBB <tall_paulb@hotmail.com>
2025-02-13 09:38:32 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -3,23 +3,38 @@
 # Eli Bendersky [https://eli.thegreenplace.net]
 # This code is in the public domain.
 
-set -eux
-set -o pipefail
 
-rm -rfv ./plugins
-rm fanboi
+#set -eux
+#set -o pipefail
+
+echo "cleaning build"
+rm -rfv ./plugins > /dev/null 2>&1
+rm fanboi  > /dev/null 2>&1
 
 
 
 mkdir plugins
 cd ./src/plugins
-go build -buildmode=plugin ./unraiddrives
-mv unraiddrives.so ../../plugins
-cd ../..
 
+echo " "
+echo "building plugins"
+for f in *; do
+    if [ -d "$f" ]; then
+        echo "             $f"
+        go build -buildmode=plugin ./$f
+         mv $f.so ../../plugins
+    fi
+done
+
+
+cd ../..
+echo " "
+echo "building main"
 cd ./src
 go build fanboi
 mv fanboi ..
 cd ..
+echo " "
+echo "done"
 
 
