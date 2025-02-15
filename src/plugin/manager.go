f996957c2d90731af995e3bb573b73a0b8d9f8e1
PaulBB <tall_paulb@hotmail.com>
2025-02-15 23:21:51 +0000

    update for plugin manager

--- src/plugin/manager.go
+++ src/plugin/manager.go
@@ -4,10 +4,6 @@
 // This code is in the public domain.
 package plugin
 
-import (
-	"fmt"
-)
-
 type PluginTemplateData struct {
 	Identifier string
 	Value      float32
@@ -20,25 +16,12 @@ type PluginInterface interface {
 	EndHook()
 }
 
-type getHook func(string) float32
-
-// the 'set' hook takes an identifier and a value (eg: fan1, 50)
-type setHook func(string, float32)
-
-type writeTemplateHook func(data map[string]float32)
-
 type PluginManager struct {
-	getHooks           map[string]getHook
-	setHooks           map[string]setHook
-	writeTemplateHooks map[string]writeTemplateHook
-	plugins            map[string]PluginInterface
+	plugins map[string]PluginInterface
 }
 
 func newPluginManager() *PluginManager {
 	pm := &PluginManager{}
-	pm.setHooks = make(map[string]setHook)
-	pm.getHooks = make(map[string]getHook)
-	pm.writeTemplateHooks = make(map[string]writeTemplateHook)
 	pm.plugins = make(map[string]PluginInterface)
 	return pm
 }
@@ -47,48 +30,10 @@ func (pm *PluginManager) RegisterPlugin(plugin string, pluginInterface PluginInt
 	pm.plugins[plugin] = pluginInterface
 }
 
-func (pm *PluginManager) RegisterGetHook(plugin string, hook getHook) {
-	pm.getHooks[plugin] = hook
-}
-
-func (pm *PluginManager) RegisterSetHook(plugin string, hook setHook) {
-	pm.setHooks[plugin] = hook
-}
-
-func (pm *PluginManager) RegisterWriteTemplateHook(plugin string, hook writeTemplateHook) {
-	pm.writeTemplateHooks[plugin] = hook
-}
-
-func (pm *PluginManager) WriteTemplateHook(plugin string, data map[string]float32) {
-	if hook, ok := pm.writeTemplateHooks[plugin]; ok {
-		hook(data)
-	} else {
-		fmt.Printf("no get hook for plugin %s' found", plugin)
-	}
-}
-
-func (pm *PluginManager) ApplyGetHook(plugin string, identifier string) (float32, error) {
-	if hook, ok := pm.getHooks[plugin]; ok {
-		return hook(identifier), nil
-	} else {
-		return -1, fmt.Errorf("no get hook for plugin %s' found", plugin)
-	}
-}
-
-func (pm *PluginManager) ApplySetHook(plugin string, identifier string, value float32) {
-	if hook, ok := pm.setHooks[plugin]; ok {
-		hook(identifier, value)
-	}
-}
-
-func (pm *PluginManager) ListTemplatePlugins() map[string]bool {
-	out := make(map[string]bool)
-	for pluginName := range pm.writeTemplateHooks {
-		out[pluginName] = true
-	}
-	return out
-}
-
 func (pm *PluginManager) GetPlugin(pluginName string) PluginInterface {
 	return pm.plugins[pluginName]
+}
+
+func (pm *PluginManager) GetPlugins() map[string]PluginInterface {
+	return pm.plugins
 }
